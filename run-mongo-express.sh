fd916e9a5f6971b4e4ff3a402d486bf3af5dede7
Alexander Rettner <alexander.rettner@googlemail.com>
2018-03-18 10:47:02 +0100

    Scripts to run MongoDB and Mongo Express in docker container

---
+++ run-mongo-express.sh
@@ -0,0 +1,8 @@
+sudo docker run \
+  --name mongo-express \
+  --network microprofile-network \
+  --link microprofile-mongo:mongo \
+  --publish 8081:8081 \
+  --rm \
+  --detach \
+  mongo-express
