b0bd0cfd8816ff778500a061d0e066712812cc81
Alexander Rettner <alexander.rettner@googlemail.com>
2018-03-19 20:37:05 +0100

    Remember commandline actions

--- run-mongo-express.sh
+++ run-mongo-express.sh
@@ -1,8 +1,8 @@
 sudo docker run \
-  --name mongo-express \
-  --network microprofile-network \
-  --link microprofile-mongo:mongo \
-  --publish 8081:8081 \
-  --rm \
-  --detach \
-  mongo-express
+    --name mongo-express \
+    --network microprofile-network \
+    --link microprofile-mongo:mongo \
+    --publish 8081:8081 \
+    --rm \
+    --detach \
+    mongo-express
--- run-mongodb.sh
+++ run-mongodb.sh
@@ -1,8 +1,8 @@
 sudo docker run \
-        --name microprofile-mongo \
-	--mount source=microprofile-mongo-volume,target=/data/db \
-	--network microprofile-network \
-	--publish 27017:27017 \
-	--detach \
-	mongo
+    --name microprofile-mongo \
+    --mount source=microprofile-mongo-volume,target=/data/db \
+    --network microprofile-network \
+    --publish 27017:27017 \
+    --detach \
+    mongo
 
